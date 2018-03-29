3628e29b5f3e8089964febadf89272160b560f32
Xinyuan <xz2566@columbia.edu>
2018-03-28 22:47:34 -0400

    Change new IP address

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.146.18:9200/"
+	ES_URL = "http://35.185.84.225:9200/"
 )
 
 
